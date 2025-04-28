358d8cea315e42eb9d04cc707447d23ca9ec3349
Devang Singh Parihar <parihardevangsingh11@gmail.com>
2025-04-28 18:03:20 +0530

    commit added

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -5,8 +5,9 @@ import Card2 from '../Card2'
 import Card3 from '../Card3'
 import Question from '../Question'
 import Gain from '../Gain'
-import Getter from '../getter'
+import Getter from '../Getter'
 import Client from '../Client'
+
 
 
 const Home = () => {
