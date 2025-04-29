c3f243dc0aa0e7652996dff5d080e100a29beb8c
Devang Singh Parihar <parihardevangsingh11@gmail.com>
2025-04-29 12:57:14 +0530

    commit added

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './main.js'
 import './index.css';
+import './App.css';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
